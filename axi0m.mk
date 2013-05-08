b9e671c0fe4195a367bd89e3fcb28a9861e68cfb
CodeTh3ory <codeth3ory@gmail.com>
2013-05-08 01:01:39 -0400

    Axi0m Rebirth

--- axi0m.mk
+++ axi0m.mk
@@ -2,20 +2,20 @@
 PRODUCT_RELEASE_NAME := GN-GSM
 
 # gsm
-$(call inherit-product, vendor/codex/config/common_gsm.mk)
+$(call inherit-product, vendor/axi0m/config/common_gsm.mk)
 
 # languages
 PRODUCT_LOCALES := en_US fr_FR it_IT es_ES de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN ru_RU ko_KR nb_NO nn_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL ka_GE lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN
 
 # phone
-$(call inherit-product, vendor/codex/config/common_phone.mk)
+$(call inherit-product, vendor/axi0m/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
-PRODUCT_NAME := codex_maguro
+PRODUCT_NAME := axi0m_maguro
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
